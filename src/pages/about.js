52d109c813ee881d022456d59fe9e7cb40a43741
Alex Votry <alexvotry@gmail.com>
2023-01-12 15:24:53 -0800

    created layout

---
+++ src/pages/about.js
@@ -0,0 +1,28 @@
+// Step 1: Import React
+import { Link } from "gatsby";
+import * as React from "react";
+import Layout from "../components/layout";
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with
+        Gatsby.
+      </p>
+    </Layout>
+  );
+};
+
+export const Head = () => (
+  <>
+    <title>About Me</title>
+    <meta name="description" content="My description" />
+  </>
+);
+//  exporting this named function inside a component like Layout won’t add the metadata to the <head>. 
+// The above works because you’re exporting Head in a page inside src/pages
+
+// Step 3: Export your component
+export default AboutPage;
